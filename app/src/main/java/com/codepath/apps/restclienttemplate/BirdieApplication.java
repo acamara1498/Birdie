e31c5ee34d89774c2c5acd5bea2f123f6b430068
Amadou Camara <acamara@college.harvard.edu>
2017-06-26 11:25:46 -0700

    name change

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/BirdieApplication.java
@@ -0,0 +1,35 @@
+package com.codepath.apps.restclienttemplate;
+
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowLog;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+import android.app.Application;
+import android.content.Context;
+
+/*
+ * This is the Android application itself and is used to configure various settings
+ * including the image cache in memory and on disk. This also adds a singleton
+ * for accessing the relevant rest client.
+ *
+ *     BirdieClient client = BirdieApplication.getRestClient();
+ *     // use client to send requests to API
+ *
+ */
+public class BirdieApplication extends Application {
+	private static Context context;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		FlowManager.init(new FlowConfig.Builder(this).build());
+		FlowLog.setMinimumLoggingLevel(FlowLog.Level.V);
+
+		BirdieApplication.context = this;
+	}
+
+	public static BirdieClient getRestClient() {
+		return (BirdieClient) BirdieClient.getInstance(BirdieClient.class, BirdieApplication.context);
+	}
+}
