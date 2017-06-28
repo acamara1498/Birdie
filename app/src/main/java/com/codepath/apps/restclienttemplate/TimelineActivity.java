28c66b58a179916bbce990b2bb240ccb71fc5260
Amadou Camara <acamara@college.harvard.edu>
2017-06-27 23:54:41 -0700

    beginning to implement networking for tweeting

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,11 +1,15 @@
 package com.codepath.apps.restclienttemplate;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.codepath.apps.restclienttemplate.models.ComposeActivity;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -41,6 +45,19 @@ public class TimelineActivity extends AppCompatActivity {
         // set the adapter
         rvTweets.setAdapter(tweetAdapter);
         populateTime();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onComposeAction(MenuItem item) {
+        //
+        Intent intent = new Intent (this, ComposeActivity.class);
+        startActivityForResult(intent, 20);
+
     }
 
     private void populateTime() {
@@ -91,4 +108,5 @@ public class TimelineActivity extends AppCompatActivity {
 
         });
     }
+
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/ComposeActivity.java
@@ -1,15 +1,90 @@
 package com.codepath.apps.restclienttemplate.models;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
+import com.codepath.apps.restclienttemplate.BirdieClient;
 import com.codepath.apps.restclienttemplate.R;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+
 
 public class ComposeActivity extends AppCompatActivity {
+
+    BirdieClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(android.R.style.Theme_DeviceDefault_Light_NoActionBar);
         setContentView(R.layout.activity_compose);
+
+        /*
+        YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
+        final String youTubeKey = getIntent().getStringExtra("youTubeKey");
+        // initialize with API key stored in secrets.xml
+        playerView.initialize(getString(R.string.youtube_api_key), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                YouTubePlayer youTubePlayer, boolean b) {
+                // do any work here to cue video, play video, etc.
+                youTubePlayer.cueVideo(youTubeKey);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                YouTubeInitializationResult youTubeInitializationResult) {
+                // log the error
+                Log.e("MovieTrailerActivity", "Error initializing YouTube player");
+            }
+        });
+        */
+
     }
+
+
+    private void createBeep(View view)
+    {
+        // take whats in the etView
+       // holder, asspciate view with tweet
+
+        client.getHomeTimeline(new JsonHttpResponseHandler()
+        {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response)
+            {
+                Log.d("TwitterClient", response.toString());
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse)
+            {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable)
+            {
+                Log.d("TwitterClient", responseString);
+                throwable.printStackTrace();
+            }
+
+
+        });
+    }
+
+
+    public void endActivity(View view) {
+
+        finish();
+    }
+
 }
